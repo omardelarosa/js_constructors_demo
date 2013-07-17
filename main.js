f87fcc05987d80f044084c176df6417423f43c56
omar delarosa <thedelarosa@gmail.com>
2013-07-17 11:18:16 -0400

    added more comments, renamed variables for the sake of semantic clarity

--- main.js
+++ main.js
@@ -36,35 +36,48 @@ function Box(content_text,color) {
     box.css('float','left');
     box.text(content_text);
     $('body').append(box);
+    // this constructor returns a Box element of type jQuery element
     return box;
 }
 
 function Ghost(interval_duration,name) {
-    var this_ghost = new Box('I am a ghost.  My name is '+name,'white');
+    // "this_ghost" is a new Object of type "Box"
+    var box_element_of_ghost = new Box('I am a ghost.  My name is '+name,'white');
     var ghost = {
         name: name,
-        element: this_ghost,
+        // ghost.element -> jQuery of newly instantiated Box.
+        element: box_element_of_ghost,
         disappear: function(){
-            this_ghost.hide(1000);
+            //takes the box element and calls plain old jQuery methods on it.
+            box_element_of_ghost.hide(1000);
         },
         reappear: function(){
-            this_ghost.show(1000);
+            //takes the box element and calls plain old jQuery methods on it.
+            box_element_of_ghost.show(1000);
         },
+        // "this" in this context refers to the "ghost" Object that we are making
+        // right now in this very literal.
         disappear_reappear : function () {
+            //makes "this" (i.e. the ghost object we are making) disappear then reappear
              this.disappear();
              this.reappear();
         }
     }
+
+    ghost.age = 5000
     //creates a dis
-    
-    timer = setInterval(function(){
+    var timer = setInterval(function(){
             ghost.disappear_reappear();
         },interval_duration)
+
     ghost.timer = timer
 
     return ghost
 }
-// var charlie_the_ghost = new Ghost(1000, "charlie")
-// var tony_the_ghost = new Ghost(5000, "tony")
+
+$(function(){
+    charlie_the_ghost = new Ghost(1000, "charlie")
+    tony_the_ghost = new Ghost(5000, "tony")
+})
 
 
